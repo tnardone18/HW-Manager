0e854089b044e2065e9b432f8c8978709e2fbe23
tnardone18 <tcnardon@syr.edu>
2026-02-07 19:53:19 +0000

    navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 st.title('Homework Manager')
 HW1 = st.Page('HW/HW1.py', title = 'HW1', icon = '💻')
 HW2 = st.Page('HW/HW2.py', title = 'HW2', icon = '💻')
-pg = st.navigation([HW1, HW2])
+HW3 = st.Page('HW/HW3.py', title = 'HW3', icon = '💻')
+pg = st.navigation([HW1, HW2, HW3])
 st.set_page_config(page_title = 'Homework Manager',
                    initial_sidebar_state='expanded')
 pg.run()
